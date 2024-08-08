4ff89d9a757956231b36c9601878086852f008e0
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2024-08-09 00:41:29 +0200

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,7 +5,6 @@
 -- country, enum of countries: US, CO and TN, never null (default=US)
 -- If table exists, script will not fail, can be executed on any database
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
        email VARCHAR(255) NOT NULL UNIQUE,
